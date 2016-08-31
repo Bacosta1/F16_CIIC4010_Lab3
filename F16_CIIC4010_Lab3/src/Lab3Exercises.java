5312415a3bf510d9746e82913743b8b063d6bbb5
4015c1 <4015c1@papaya.ece.uprm.edu>
2016-08-31 12:03:09 -0400

    Changed the name, location and size

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame Bryan = new JFrame("Hello, world!");
 		Bryan.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		Bryan.setLocation(875, 420);
+		Bryan.setLocation(865, 420);
 		Bryan.setSize(200, 200);
 		Bryan.setVisible(true);
 	}
