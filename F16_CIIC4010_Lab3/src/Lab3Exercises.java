0909b0304db1f5a79d324910593d0e237a9f6c41
4015c1 <4015c1@papaya.ece.uprm.edu>
2016-08-31 11:59:23 -0400

    Changed the size,l location and name

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
+		JFrame Bryan = new JFrame("Hello, world!");
+		Bryan.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Bryan.setLocation(875, 420);
+		Bryan.setSize(200, 200);
+		Bryan.setVisible(true);
 	}
 }
